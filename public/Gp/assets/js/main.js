574301bd33cd98c70eba19296deef80fd9515eef
Foram <Foramgandhi12@yahoo.ca>
2020-07-23 13:11:27 -0400

    Update main.js

--- public/Gp/assets/js/main.js
+++ public/Gp/assets/js/main.js
@@ -253,6 +253,7 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  if slides:
+      slides[slideIndex-1].style.display = "block";
+      dots[slideIndex-1].className += " active";
 }
